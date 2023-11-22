65bb4f6c86974c94e9d46d1e87d2aea4c8c98e37
Ahmed <a7medalqalawyi@bmail.com>
2023-11-22 07:50:29 +0200

    complete recover password screen

--- lib/presentation/screens/optm_screen.dart
+++ lib/presentation/screens/optm_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:the_black_store/core/utils/routes.dart';
-
-import '../../core/utils/app_constants.dart';
-import '../widgets/leading_app_bar.dart';
 class OTPScreen extends StatelessWidget {
   const OTPScreen({super.key});
 
